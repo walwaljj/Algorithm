37812041800d566127ff59759447c7a750a40be3
Shjung <wjdtngus9@gmail.com>
2023-05-02 09:11:19 +0900

    programmers 하샤드 수

---
+++ src/main/java/jsh/algorithm/programmers/lessons12947.java
@@ -0,0 +1,22 @@
+package jsh.algorithm.programmers;
+
+public class lessons12947 {
+    public boolean solution(int x) {
+
+        int div = 0;
+        int numSum = 0;
+        int y = x;
+        while (x > 0){
+            div = x % 10;
+            numSum += div;
+            x /= 10;
+        }
+
+        if(y % numSum != 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+}

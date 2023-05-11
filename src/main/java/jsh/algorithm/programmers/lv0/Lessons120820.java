96a3db2c7322a3dceea288fc1681901ae4f97cac
Shjung <wjdtngus9@gmail.com>
2023-05-11 22:56:24 +0900

    Programmers 나이계산

---
+++ src/main/java/jsh/algorithm/programmers/lv0/Lessons120820.java
@@ -0,0 +1,20 @@
+package jsh.algorithm.programmers.lv0;
+
+
+public class Lessons120820 {
+
+    public static void main(String[] args) {
+        Lessons120820 result = new Lessons120820();
+
+        System.out.println(result.solution(23));
+    }
+
+    public int solution(int age) {
+        int answer = 0;
+        answer = 2022 - age + 1 ;
+
+        return answer;
+    }
+
+
+}

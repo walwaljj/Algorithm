8fdf1c4a094a9b372a7373953e977fefea0c4c91
Shjung <wjdtngus9@gmail.com>
2023-05-14 16:33:13 +0900

    Baekjoon 1

---
+++ src/main/java/jsh/algorithm/baekjoon/Baekjoon4375.java
@@ -0,0 +1,27 @@
+package jsh.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/4375
+ * 1
+ */
+public class Baekjoon4375 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()){
+            int n = sc.nextInt();
+            int num = 0;
+            for (int count = 1; ; count++) {
+                num = num * 10 + 1;
+                num %= n;
+                if(num == 0){
+                    System.out.println(count);
+                    break;
+                }
+
+            }
+        }
+
+    }
+}

8beb060b761d67c5cc8ae07370918f10a90a4ee1
Shjung <wjdtngus9@gmail.com>
2023-05-16 23:12:45 +0900

    Baekjoon 시험 성적

---
+++ src/main/java/jsh/algorithm/baekjoon/Baekjoon9498.java
@@ -0,0 +1,27 @@
+package jsh.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+/*
+* https://www.acmicpc.net/problem/9498
+* 시험 성적
+* */
+public class Baekjoon9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String str = "";
+        if(num >= 90){
+            str = "A";
+        } else if (num >= 80) {
+            str = "B";
+        } else if (num >= 70) {
+            str = "C";
+        }else if (num >= 60) {
+            str = "D";
+        }else {
+            str = "F";
+        }
+        System.out.println(str);
+    }
+}

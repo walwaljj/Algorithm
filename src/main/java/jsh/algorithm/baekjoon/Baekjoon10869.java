db197e066c739662df6b2dc58bc68bfb9c71ab8a
Shjung <wjdtngus9@gmail.com>
2023-05-16 23:12:51 +0900

    Baekjoon 사칙연산

---
+++ src/main/java/jsh/algorithm/baekjoon/Baekjoon10869.java
@@ -0,0 +1,19 @@
+package jsh.algorithm.baekjoon;
+
+import java.util.Scanner;
+/*
+* https://www.acmicpc.net/problem/10869
+* 사칙연산
+* */
+public class Baekjoon10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}

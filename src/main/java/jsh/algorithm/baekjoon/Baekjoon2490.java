2f27ceb724776b9d6dc5c763e14c0be5101e100f
Shjung <wjdtngus9@gmail.com>
2023-05-16 23:12:22 +0900

    Baekjoon 윷놀이

---
+++ src/main/java/jsh/algorithm/baekjoon/Baekjoon2490.java
@@ -0,0 +1,35 @@
+package jsh.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+/*
+* https://www.acmicpc.net/problem/2490
+* 윷놀이
+* */
+public class Baekjoon2490 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        int count = 0;
+
+        for(int i = 0 ; i < 3 ; i ++){
+            for(int j = 0 ; j < 4 ; j ++){
+                count += sc.nextInt();
+            }
+            if(count == 4){
+                str += "E\n";
+            } else if (count == 1) {
+                str += "C\n";
+            } else if (count == 2) {
+                str += "B\n";
+            } else if (count == 3) {
+                str += "A\n";
+            }else if(count == 0){
+                str += "D\n";
+            }
+            count = 0;
+        }
+        System.out.println(str);
+    }
+
+}

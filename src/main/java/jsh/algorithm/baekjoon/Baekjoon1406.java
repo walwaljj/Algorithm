63f0a27b2c321ad57aa32d2a612ccc73fb6b3c21
Shjung <wjdtngus9@gmail.com>
2023-06-03 21:29:57 +0900

    Baekjoon 에디터 (StringBuilder 시간 초과)

---
+++ src/main/java/jsh/algorithm/baekjoon/Baekjoon1406.java
@@ -0,0 +1,39 @@
+package jsh.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/1406
+ * 에디터
+ */
+public class Baekjoon1406 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        String cmd = "";
+        int cursor = sb.length();
+        for (int i = 0; i < n; i++) {
+            cmd = br.readLine();
+            if(cmd.contains("P")){
+                sb.insert(cursor,cmd.split(" ")[1]);
+                cursor++;
+            } else if (cmd.contains("L")) {
+                cursor = cursor > 0 ? cursor -1 : 0;
+            } else if (cmd.contains("B")) {
+                if(cursor > 0){
+                 sb.delete(cursor-1,cursor) ;
+                 cursor--;
+                }
+            } else if (cmd.contains("D")) {
+            cursor = cursor < sb.length() ? cursor +1 : sb.length();
+        }
+    }
+
+        System.out.println(sb);
+    }
+}

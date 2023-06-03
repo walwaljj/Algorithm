42022f2849db0368c1c834dec8c1a08a39fb966c
Shjung <wjdtngus9@gmail.com>
2023-06-04 00:57:41 +0900

    Rename : Baekjoon 에디터 (시간초과)
    백준 폴더 구조 변경

--- src/main/java/jsh/algorithm/baekjoon/silver/Baekjoon1406.java
+++ src/main/java/jsh/algorithm/baekjoon/silver/Baekjoon1406.java
@@ -1,51 +1,39 @@
-package jsh.algorithm.baekjoon;
+package jsh.algorithm.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * https://www.acmicpc.net/problem/1406
  * 에디터
  */
-public class Baekjoon1406_2 {
+public class Baekjoon1406 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Character> list = new Stack();
-//        LinkedList<String> list = new LinkedList<>();
-        String str = br.readLine();
-        for (int i = 0; i < str.length(); i++) {
-            list.add(str.charAt(i));
-        }
-
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine());
         int n = Integer.parseInt(br.readLine());
         String cmd = "";
-        int cursor = list.size();
-
+        int cursor = sb.length();
         for (int i = 0; i < n; i++) {
             cmd = br.readLine();
             if(cmd.contains("P")){
-                list.add(cursor,cmd.charAt(2));
-                cursor ++;
-
+                sb.insert(cursor,cmd.split(" ")[1]);
+                cursor++;
             } else if (cmd.contains("L")) {
                 cursor = cursor > 0 ? cursor -1 : 0;
-
             } else if (cmd.contains("B")) {
                 if(cursor > 0){
-                 list.remove(cursor-1);
+                 sb.delete(cursor-1,cursor) ;
                  cursor--;
                 }
-
             } else if (cmd.contains("D")) {
-            cursor = cursor < list.size() ? cursor +1 : list.size();
+            cursor = cursor < sb.length() ? cursor +1 : sb.length();
         }
     }
-        for (Character character : list) {
-            System.out.print(character);
-        }
+
+        System.out.println(sb);
     }
 }
--- src/main/java/jsh/algorithm/baekjoon/silver/Baekjoon1406_2.java
+++ src/main/java/jsh/algorithm/baekjoon/silver/Baekjoon1406_2.java
@@ -1,39 +1,51 @@
-package jsh.algorithm.baekjoon;
+package jsh.algorithm.baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * https://www.acmicpc.net/problem/1406
  * 에디터
  */
-public class Baekjoon1406 {
+public class Baekjoon1406_2 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        sb.append(br.readLine());
+        Stack<Character> list = new Stack();
+//        LinkedList<String> list = new LinkedList<>();
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        }
+
         int n = Integer.parseInt(br.readLine());
         String cmd = "";
-        int cursor = sb.length();
+        int cursor = list.size();
+
         for (int i = 0; i < n; i++) {
             cmd = br.readLine();
             if(cmd.contains("P")){
-                sb.insert(cursor,cmd.split(" ")[1]);
-                cursor++;
+                list.add(cursor,cmd.charAt(2));
+                cursor ++;
+
             } else if (cmd.contains("L")) {
                 cursor = cursor > 0 ? cursor -1 : 0;
+
             } else if (cmd.contains("B")) {
                 if(cursor > 0){
-                 sb.delete(cursor-1,cursor) ;
+                 list.remove(cursor-1);
                  cursor--;
                 }
+
             } else if (cmd.contains("D")) {
-            cursor = cursor < sb.length() ? cursor +1 : sb.length();
+            cursor = cursor < list.size() ? cursor +1 : list.size();
         }
     }
-
-        System.out.println(sb);
+        for (Character character : list) {
+            System.out.print(character);
+        }
     }
 }

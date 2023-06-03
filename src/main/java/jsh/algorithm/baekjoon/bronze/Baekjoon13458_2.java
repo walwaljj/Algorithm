baa151b7e634049709b52d2196141082100422b6
Shjung <wjdtngus9@gmail.com>
2023-06-04 01:55:56 +0900

    Rename : Baekjoon 시험 감독
    백준 폴더 구조 변경

--- src/main/java/jsh/algorithm/baekjoon/bronze/Baekjoon13458_2.java
+++ src/main/java/jsh/algorithm/baekjoon/bronze/Baekjoon13458_2.java
@@ -1,4 +1,4 @@
-package jsh.algorithm.baekjoon;
+package jsh.algorithm.baekjoon.bronze;
 
 import java.io.*;
 
@@ -9,15 +9,14 @@ public class Baekjoon13458_2 {
         int locationNum = Integer.parseInt(br.readLine());
         String[] taskerArr = br.readLine().split(" ");
         String[] supervisorArr = br.readLine().split(" ");
-        int count = 0;
-        int remain = 0;
+        long count = 0l;
+        long remain = 0l;
 
         for (int i = 0; i < locationNum; i++) {
-            remain = Integer.parseInt(taskerArr[i]) - Integer.parseInt(supervisorArr[0]);
-            while (remain > 0){
-                remain -= Integer.parseInt(supervisorArr[1]);
-                count++ ;
-            }
+            remain = Long.parseLong(taskerArr[i]) - Long.parseLong(supervisorArr[0]) > 0 ?
+                    Long.parseLong(taskerArr[i]) - Long.parseLong(supervisorArr[0]) : 0;
+            count += remain / Long.parseLong(supervisorArr[1]);
+            count += remain % Long.parseLong(supervisorArr[1]) == 0l ? 0l : 1l ;
         }
 
         System.out.println(count + locationNum);

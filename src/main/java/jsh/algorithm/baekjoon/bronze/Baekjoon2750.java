e6eaa15a7cfa1db75f4b285f56c9817f7f7dba24
Shjung <wjdtngus9@gmail.com>
2023-06-09 18:07:54 +0900

    Rename : Baekjoon 수 정렬하기
    파일 이름 변경

--- src/main/java/jsh/algorithm/baekjoon/bronze/Baekjoon2750.java
+++ src/main/java/jsh/algorithm/baekjoon/bronze/Baekjoon2750.java
@@ -1,16 +1,14 @@
 package jsh.algorithm.baekjoon.bronze;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**https://www.acmicpc.net/problem/2750
  * 수 정렬하기
  */
-public class Backjoon2750 {
+public class Baekjoon2750 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());

c353878988f75408dc00a4cdeeb2064f5a7b92ef
Shjung <wjdtngus9@gmail.com>
2023-06-20 17:44:54 +0900

    Feat : baekjoon N 과 M (8)

---
+++ src/main/java/jsh/algorithm/baekjoon/silver/Baekjoon15657.java
@@ -0,0 +1,59 @@
+package jsh.algorithm.baekjoon.silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/15657
+ * N과 M (8)
+ */
+public class Baekjoon15657 {
+
+    static int n , m;
+    static int[] arr = new int[9], temp = new int[9];
+    static boolean[] chk = new boolean[9];
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int point){
+        if(point == m){
+            for (int i = 0; i < m; i++) {
+                sb.append(temp[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n ; i++) {
+            if(!chk[i]){
+                temp[point] = arr[i - 1];
+
+                dfs(point + 1);
+                chk[i] = true;
+
+                for (int j = 1 + 1; j <= n; j++) {
+                    if( i < j){
+                        chk[j] = false;
+                    }
+
+                }
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        arr = Arrays.copyOf(arr,n);
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+
+    }
+}
